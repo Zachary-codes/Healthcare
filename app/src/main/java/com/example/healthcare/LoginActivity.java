ed437f5735ef5eacd217ba055717534821f88333
Zachary Langeveld <66338161+Zacharycodez@users.noreply.github.com>
2024-01-30 10:27:58 +0200

    Healthcare Mobile App in Android

--- app/src/main/java/com/example/healthcare/LoginActivity.java
+++ app/src/main/java/com/example/healthcare/LoginActivity.java
@@ -3,8 +3,15 @@ package com.example.healthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
